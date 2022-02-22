9fe1a889c2f868a7d1cb58ffec558c6909cd8aba
manutdmohit <saudmohit@gmail.com>
2022-02-22 11:27:07 +0545

    controllers

---
+++ controllers/auth.js
@@ -0,0 +1,7 @@
+exports.register = async (req, res) => {
+  res.send('register user');
+};
+
+exports.login = async (req, res) => {
+  res.send('login user');
+};
---
+++ controllers/jobs.js
@@ -0,0 +1,19 @@
+exports.getAllJobs = async (req, res) => {
+  res.send('get all jobs');
+};
+
+exports.getJob = async (req, res) => {
+  res.send('get job');
+};
+
+exports.createJob = async (req, res) => {
+  res.send('create job');
+};
+
+exports.updateJob = async (req, res) => {
+  res.send('Update Job');
+};
+
+exports.deleteJob = async (req, res) => {
+  res.send('Delete Job');
+};
