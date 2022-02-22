de640943eedac854a44d71a87e2975fb9eb9bbe6
manutdmohit <saudmohit@gmail.com>
2022-02-22 13:45:04 +0545

    User Model

--- models/User.js
+++ models/User.js
@@ -1,24 +1,18 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Please provide name'],
-    minlength: 3,
     maxlength: 50,
+    minlength: 3,
   },
   email: {
     type: String,
     required: [true, 'Please provide email'],
-    minlength: 3,
-    maxlength: 50,
-  },
-  name: {
-    type: String,
-    required: [true, 'Please provide name'],
     match: [
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      'Please provide email',
+      'Please provide a valid email',
     ],
     unique: true,
   },
@@ -26,8 +20,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide password'],
     minlength: 6,
-    maxlength: 12,
   },
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', UserSchema);
