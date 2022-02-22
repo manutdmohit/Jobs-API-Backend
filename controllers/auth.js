63f9ac2a30561b906ff88b85f569b8db29898b95
manutdmohit <saudmohit@gmail.com>
2022-02-22 20:54:40 +0545

    Generate Token Instance Method

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,4 @@
 const { StatusCodes } = require('http-status-codes');
-const jwt = require('jsonwebtoken');
 
 const { BadRequestError } = require('../errors');
 
@@ -8,9 +7,7 @@ const User = require('../models/User');
 exports.register = async (req, res) => {
   const user = await User.create({ ...req.body });
 
-  const token = jwt.sign({ userId: user._id, name: user.name }, 'jwtsecret', {
-    expiresIn: '30d',
-  });
+  const token = user.generateToken();
 
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema(
   {
@@ -33,5 +34,11 @@ UserSchema.pre('save', async function () {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+UserSchema.methods.generateToken = function () {
+  return jwt.sign({ userId: this._id, name: this.name }, 'jwtsecret', {
+    expiresIn: '30d',
+  });
+};
 
 module.exports = mongoose.model('User', UserSchema);
