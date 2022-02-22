ec2e4d716a9b3eb829a59b64a51e45fabb1fa7a4
manutdmohit <saudmohit@gmail.com>
2022-02-22 16:34:09 +0545

    Generate Token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,18 @@
 const { StatusCodes } = require('http-status-codes');
-const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const { BadRequestError } = require('../errors');
 
 const User = require('../models/User');
 
 exports.register = async (req, res) => {
   const user = await User.create({ ...req.body });
 
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = jwt.sign({ userId: user._id, name: user.name }, 'jwtsecret', {
+    expiresIn: '30d',
+  });
+
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 exports.login = async (req, res) => {
