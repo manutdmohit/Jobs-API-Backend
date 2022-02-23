f208e362576458635ebdc0e1ecc1402ce6432464
manutdmohit <saudmohit@gmail.com>
2022-02-23 10:46:58 +0545

    Alternative code for req.user

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -14,9 +14,14 @@ const auth = async (req, res, next) => {
 
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
-    console.log(payload);
 
     // attach the user to the job routes
+
+    // alternative code
+    // const user = await User.findById(payload.userId).select('-password');
+
+    // req.user = user;
+
     req.user = { userId: payload.userId, name: payload.name };
 
     next();
