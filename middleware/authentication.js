cecd87a2e3fcf66e2d6957f1655fd873683079b7
manutdmohit <saudmohit@gmail.com>
2022-02-23 20:42:51 +0545

    Custom Error

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const User = require('../models/User');
+// const User = require('../models/User');
 const { UnauthenticatedError } = require('../errors');
 
 const auth = async (req, res, next) => {
